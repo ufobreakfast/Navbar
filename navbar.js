45c476bf110c10e1dab414c3043114e2a18c3311
GEORGE M <gmealy26@gmail.com>
2024-03-12 14:07:04 -0700

    Create navbar.js

---
+++ navbar.js
@@ -0,0 +1,11 @@
+// Initialize the dropdown toggle
+$('.dropdown-toggle').dropdown();
+
+// Add animation on show/hide events
+$('.dropdown').on('show.bs.dropdown', function () {
+  $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+});
+
+$('.dropdown').on('hide.bs.dropdown', function () {
+  $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+});
